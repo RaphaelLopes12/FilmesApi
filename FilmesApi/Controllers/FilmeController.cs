70f514a05f3e7ee2a7add0f8dfa31239ddcd4b6a
Raphael Ferreira Lopes <raphael.lopes@senior.com.br>
2023-12-21 12:56:56 -0300

    Início do projeto API Fillmes.

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using FilmesApi.Data;
+using FilmesApi.Data.Dtos;
+using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private FilmeContext _context;
+    private IMapper _mapper;
+
+    public FilmeController(FilmeContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public IActionResult AdicionaFilme([FromBody] CreateFilmeDto filmeDto)
+    {
+        Filme filme = _mapper.Map<Filme>(filmeDto);
+        _context.Filmes.Add(filme);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(RecuperaFilmeId), new { id = filme.Id}, filme);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmne([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    {
+        return _context.Filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmeId(int id)
+    {
+        var filme = _context.Filmes.FirstOrDefault(filmes => filmes.Id == id);
+        if (filme == null) return NotFound();
+        return Ok(filme);
+    }
+}
